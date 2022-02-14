da7289539d794e327b91080efb0ee5347a15312a
DYBEDY <rd.iosdeveloper@gmail.com>
2022-02-14 22:36:39 +0300

    Initial Commit

---
+++ TraficLight/TraficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightApp.swift
+//  TraficLight
+//
+//  Created by Roman on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
